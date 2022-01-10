77dae97eaafa07f76ce756c02d5dde370637b650
wing0221 <pokeanto0221@gmail.com>
2022-01-10 18:12:20 +0900

    login routes

---
+++ components/LoggedIn/LoggedInAppBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app-bar
+    app
+    dense
+    elevation="1"
+    clipped-left
+    color="white"
+  >
+    <nuxt-link
+      to="redirectPath"
+      class="text-decoration-none"
+    >
+      <app-logo />
+    </nuxt-link>
+
+    <app-title />
+    <v-spacer />
+    <before-login-app-signup-button />
+    <before-login-app-login-button />
+    <logged-in-app-bar-account-menu />
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ pages/projects.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    project.vue
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'logged-in'
+}
+</script>
